6daf51eca39058928c7f2f3333f58ac33268d194
AntonyMo <32247865+AntonyMoes@users.noreply.github.com>
2021-02-18 15:03:23 +0300

    Separated movement system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,62 +4,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    public float acceleration = 10;
-    public float deceleration = 15;
-    public float speed = 8;
-    public float rotationSpeed = 360;
-
-    Rigidbody2D rb;
-    float _verticalInput;
+    ThrusterSystem _thrusters;
     float _horizontalInput;
+    float _verticalInput;
     Vector3 _mousePos;
 
     void Start() {
-        rb = GetComponent<Rigidbody2D>();
+        _thrusters = GetComponent<ThrusterSystem>();
     }
 
     void Update() {
-        _verticalInput = Input.GetAxisRaw("Vertical");
         _horizontalInput = Input.GetAxisRaw("Horizontal");
+        _verticalInput = Input.GetAxisRaw("Vertical");
         
         _mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 
     void FixedUpdate() {
-        
-        Move(_verticalInput, _horizontalInput);
+        var inputs = new Vector2(_horizontalInput, _verticalInput);
+        _thrusters.Move(inputs);
         
         var direction = ((Vector2) (_mousePos - transform.position)).normalized;
-        Turn(direction);
-    }
-
-    void Move(float verticalInput, float horizontalInput) {
-        var input = new Vector2(horizontalInput, verticalInput);
-        Vector2 accelerationVector = Vector2.zero;
-        if (input.magnitude != 0) {
-            accelerationVector = input.normalized * acceleration;
-        }
-        else {
-            accelerationVector = rb.velocity.normalized * (deceleration * -1);
-        }
-        var speedVector = rb.velocity + accelerationVector * Time.fixedDeltaTime;
-        rb.velocity = ApplySpeedConstraint(speedVector, speed);
-    }
-
-    Vector2 ApplySpeedConstraint(Vector2 speed, float maxSpeed) {
-        if (speed.magnitude <= maxSpeed) {
-            return speed;
-        }
-
-        return speed.normalized * maxSpeed;
-    }
-
-    void Turn(Vector2 direction) {
-        var rawAngle = Vector2.SignedAngle(transform.up, direction);
-        var turnDirection = Mathf.Sign(rawAngle);
-        var maxRotation = rotationSpeed * Time.deltaTime;
-
-        var angle = turnDirection * Mathf.Min(Mathf.Abs(rawAngle), maxRotation);
-        transform.Rotate(Vector3.forward, angle);
+        _thrusters.Turn(direction);
     }
 }
---
+++ Assets/Scripts/ThrusterSystem.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrusterSystem : MonoBehaviour {
+    [HideInInspector] public Vector2 moveInput;
+    [HideInInspector] public Vector2 directionInput;
+    public float acceleration = 10;
+    public float deceleration = 15;
+    public float speed = 8;
+    public float rotationSpeed = 1440;
+
+    Rigidbody2D rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // void FixedUpdate()
+    // {
+    //     Move(moveInput);
+    //     Turn(directionInput);
+    // }
+    
+    public void Move(Vector2 input) {
+        Vector2 accelerationVector;
+        if (input.magnitude != 0) {
+            accelerationVector = input.normalized * acceleration;
+        }
+        else {
+            accelerationVector = rb.velocity.normalized * (deceleration * -1);
+        }
+        var speedVector = rb.velocity + accelerationVector * Time.fixedDeltaTime;
+        rb.velocity = ApplySpeedConstraint(speedVector, speed);
+    }
+
+    Vector2 ApplySpeedConstraint(Vector2 speed, float maxSpeed) {
+        if (speed.magnitude <= maxSpeed) {
+            return speed;
+        }
+
+        return speed.normalized * maxSpeed;
+    }
+
+    public void Turn(Vector2 direction) {
+        var rawAngle = Vector2.SignedAngle(transform.up, direction);
+        var turnDirection = Mathf.Sign(rawAngle);
+        var maxRotation = rotationSpeed * Time.deltaTime;
+
+        var angle = turnDirection * Mathf.Min(Mathf.Abs(rawAngle), maxRotation);
+        transform.Rotate(Vector3.forward, angle);
+    }
+}
