a41e40b6e832767be4b2ed60fc9a35083c7374df
AntonyMo <32247865+AntonyMoes@users.noreply.github.com>
2021-02-24 00:26:06 +0300

    Implemented simple health system

---
+++ Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class HealthSystem : MonoBehaviour {
+    [SerializeField] float maxHealth;
+    public float MaxHealth => maxHealth;
+    float _currentHealth;
+    public float CurrentHealth => _currentHealth;
+
+    void Start() {
+        _currentHealth = maxHealth;
+    }
+    
+    public void InflictDamage(float damage) {
+        _currentHealth -= Mathf.Min(_currentHealth, damage);
+        if (_currentHealth == 0) {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/ProjectileCollide.cs
+++ Assets/Scripts/ProjectileCollide.cs
@@ -1,13 +1,33 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ProjectileCollide : MonoBehaviour {
-    // public float damage;
-    // public float radius = 0;
+    public float damage;
+    public float radius;
 
     void OnCollisionEnter2D(Collision2D other) {
         Destroy(gameObject);
+        var targets = new List<HealthSystem>();
+
+        if (radius == 0) {
+            var targetHealth = other.gameObject.GetComponent<HealthSystem>();
+            if (targetHealth) {
+                targets.Add(targetHealth);
+            }
+        }
+        else {
+            var entitiesWithHealth = 
+                Physics2D.OverlapCircleAll(transform.position, radius)
+                .Select(c => c.gameObject.GetComponent<HealthSystem>()).ToArray()
+                .Where(h => h);
+            targets.AddRange(entitiesWithHealth);
+        }
+
+        foreach (var target in targets) {
+            target.InflictDamage(damage);
+        }
     }
 }
